b596c9220a93f843df63052df3f1481e7fcacbd4
nurfaidzy <nurfaidzy@gmail.com>
2023-01-29 18:45:04 +0700

    feat(login)
    fitur untuk melakukan login

--- Components/Auth/Login.js
+++ Components/Auth/Login.js
@@ -1,9 +1,30 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const userServices = require('../Services/UserServices');
 
 const secret = process.env.SECRET_KEY;
 
-module.exports.login = (req, res) => {
-  const user = { id: 1, username: 'johndoe' };
-  const token = jwt.sign(user, secret);
-  res.json({ token });
+module.exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const findUser = await userServices.findEmailareSame({ email });
+
+    if (!findUser) {
+      return res.status(401).json({ error: 'Email Not Found' });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, findUser.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ error: 'Password Wrong' });
+    }
+
+    const user = { id: findUser.id, email: findUser.email };
+    const token = jwt.sign(user, secret, { expiresIn: '1h' });
+
+    return res.json({ token });
+  } catch (error) {
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
 };
