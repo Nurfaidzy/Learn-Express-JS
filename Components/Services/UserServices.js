5457f54d65fd3b36e8cbd436969e0a7bf1328086
nurfaidzy <nurfaidzy@gmail.com>
2023-02-05 14:08:12 +0700

    feat (user reponse data)
    adding user role on back

--- Components/Services/UserServices.js
+++ Components/Services/UserServices.js
@@ -1,6 +1,14 @@
 const user = require('../Model/User');
+const access = require('../Model/Access');
 
-module.exports.getUsers = () => user.findAll({ attributes: ['username', 'email'] });
+module.exports.getUsers = () => user.findAll({
+  attributes: ['id', 'username', 'email'],
+  include: [{
+    model: access,
+    as: 'accessRole',
+    attributes: ['name_access'],
+  }],
+});
 
 module.exports.newUser = (payload) => user.create({
   username: payload.username,
